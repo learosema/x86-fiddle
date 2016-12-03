86e7884b2e11730da0a4a86a2f552010ea266bb4
Lutz Rosema <terabaud@gmail.com>
2016-12-03 03:37:18 +0100

    remove js-dos api call, set editor font size

--- app.js
+++ app.js
@@ -3,21 +3,9 @@ const editor = ace.edit("editor")
 editor.setTheme("ace/theme/monokai")
 editor.getSession().setMode("ace/mode/assembly_x86")
 editor.getSession().setTabSize(8)
+q('#editor').style.fontSize="3vh"
 editor.setValue(q('#demoCode').textContent)
 
-const HelloWorldDotCom = "X5O!P%@AP[4\PZX54(P^)7CC)7}$HELLO-WORLD!$H+H*"
-
-function run() {
-	var dosbox = new Dosbox({
-           id: "dosbox",
-           onload: function (dosbox) {
-             dosbox.run("https://js-dos.com/cdn/digger.zip", "./DIGGER.COM")
-           },
-           onrun: function (dosbox, app) {
-             console.log("App '" + app + "' is runned")
-           }
-         });
-}
 
 q('#btnRun').onclick = () => {
 	DOS.init().then(() => DOS.mountZip("https://terabaud.github.io/x86-fiddle/dos.zip")).then(() => DOS.run("./ANIM.COM"))

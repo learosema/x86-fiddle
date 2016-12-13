f4201edacf0eca5a8614479c7e76cd6cecfa8c5b
Lutz Rosema <terabaud@gmail.com>
2016-12-13 16:43:32 +0100

    added loader.js

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const editor = ace.edit("editor")
 editor.setTheme("ace/theme/monokai")
 editor.getSession().setMode("ace/mode/assembly_x86")
 editor.getSession().setTabSize(8)
-q('#editor').style.fontSize="3vh"
-editor.setValue(q('#demoCode').textContent)
+editor.setValue(q('#demoCode').textContent, -1)
 
 
 q('#btnRun').onclick = () => {
---
+++ loader.js
@@ -0,0 +1,48 @@
+const http = (url) => { 
+  const xhr = new XMLHttpRequest() 
+  if (url.slice(-4) === ".zip") { 
+    // xhr.responseType = "arraybuffer" 
+  } 
+  xhr.open("GET", url, true) 
+  xhr.overrideMimeType('text/plain; charset=x-user-defined') 
+  xhr.send() 
+  return new Promise((yep, nope) => { 
+    xhr.onreadystatechange = () => { 
+      if (xhr.readyState != 4) return 
+      (xhr.status == 200 ? yep : nope)(xhr.responseText) 
+    } 
+  }) 
+}
+
+const stringToByteArray = function(data) {
+  // this is a bit hacky (todo: use binary xhr2 fetching)
+  if (typeof data === 'string') { 
+    return Array(data.length).fill(0).map((e,i) => data.charCodeAt(i)) 
+  } 
+  return data; 
+} 
+
+const CDN = 'https://terabaud.github.io/x86-fiddle/dosbox.js'
+
+const DOS = {
+	module: { 
+    	canvas: document.querySelector('.dosbox-canvas'), 
+    	preRun: [], 
+    	postRun: [], 
+    	print: console.log.bind(), 
+    	printErr: console.error.bind(), 
+    	setStatus: console.log.bind(), 
+    	monitorRunDependencies: (left) => { 
+            DOS.module.totalDependencies = Math.max(DOS.totalDependencies, left); 
+            const status = left ? "Preparing... (" + (DOS.totalDependencies - left) + "/" + DOS.totalDependencies + ")" : 'All downloads complete.'; 
+            return DOS.module.setStatus(status); 
+    	} 
+	},
+	run: function() {
+		
+		http(CDN).then(script => {
+			const Module = DOS.module
+			
+		})
+	}
+}
